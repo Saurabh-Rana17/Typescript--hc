242c2e562415f97f3050ac0e300bee58d2936c85
gmr1289 <gmrgamer1289@gmail.com>
2024-05-31 00:07:14 +0530

    enum

--- Enum/enum.js
+++ Enum/enum.js
@@ -1,2 +1,17 @@
-var role;
-role = "admin";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// let role: "teacher" | "student" | "hod";
+// role = "teacher";
+var tup;
+tup = ["dhsjds", "232", 23, 3213];
+tup.pop();
+tup.push("dsd");
+// console.log(tup);
+var Role;
+(function (Role) {
+    Role["teacher"] = "dsi";
+    Role[Role["admin"] = 5] = "admin";
+    Role[Role["studentr"] = 6] = "studentr";
+})(Role || (Role = {}));
+var user = Role.studentr;
+console.log(user);
--- Enum/enum.ts
+++ Enum/enum.ts
@@ -4,3 +4,18 @@ export {};
 
 let tup: [string, string, number, number];
 tup = ["dhsjds", "232", 23, 3213];
+tup.pop();
+tup.push("dsd");
+// console.log(tup);
+
+enum Role {
+  teacher = "dsi",
+  admin = 5,
+  studentr,
+}
+
+let user = Role.studentr;
+console.log(user);
+const obj: { key: string } = {
+  key: "",
+};
